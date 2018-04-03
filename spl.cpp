d3c639db2fd0a2ff7696737aed15de66f0aa6ac2
MDAminul01100 <35560083+MDAminul01100@users.noreply.github.com>
2018-04-03 13:04:52 +0600

    Add files via upload

--- spl.cpp
+++ spl.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<sstream>
 #include<bitset>
+#include<iomanip>
 
 using namespace std;
 
@@ -37,6 +38,7 @@ string paddingMessage(string str)
     int bitlen = strlen(msgNew);
 
     msgNew += '1';
+
     int padcount = (448 - ((bitlen % 512) + 1));
 
     for (int i = 0; i < padcount; i++)
@@ -51,10 +53,118 @@ string paddingMessage(string str)
     return msgNew;
 }
 
+bitset<32> rotateRight(bitset<32> bits,int n)
+{
+    return (bits >> n) | (bits << (32-n));
+}
+
+bitset<32> Ch(bitset<32> x,bitset<32> y,bitset<32> z)
+{
+    return (x & y) ^ (~x & z);
+}
+
+bitset<32> Maj(bitset<32> x, bitset<32> y, bitset<32> z)
+{
+    return (x & y) ^ (y & z) ^ (z & x);
+}
+
+bitset<32> add(bitset<32> a, bitset<32> b)
+{
+    unsigned long c = a.to_ulong();
+    unsigned long d = b.to_ulong();
+    return bitset<32>((c + d) % 4294967296);
+
+}
+
+string binToHex(string hashInBin)
+{
+    string digits[16] = {"0","1","2","3", "4", "5", "6", "7","8", "9", "A", "B", "C", "D", "E", "F"};
+    //int x = 0;
+    string str;
+    for(int i = 0; i < hashInBin.size(); i+=4)
+    {
+        string subString = hashInBin.substr(i,4);
+        int x = 0;
+        if(subString.substr(0,1) == "1") x += 8;
+        if(subString.substr(1,1) == "1") x += 4;
+        if(subString.substr(2,1) == "1") x += 2;
+        if(subString.substr(3,1) == "1") x += 1;
+
+        str += digits[x];
+    }
+    return str;
+}
+
+
+string makingDigest(string messege)
+{
+    bitset<32> words[64];
+
+    for(int i = 0,counter = 0;i < 16; i++)
+    {
+        for(int j = 15; j >= 0; j--)
+        {
+            words[i][j] = messege[counter];
+            counter++;
+        }
+    }
+    for (int k = 16; k < 64; k++)
+    {
+        bitset<32> s0 = rotateRight(words[k - 15], 7) ^ rotateRight(words[k - 15], 18) ^ (words[k - 15] >> 3);
+        bitset<32> s1 = rotateRight(words[k - 2], 17) ^ rotateRight(words[k - 2], 19) ^ (words[k - 2] >> 10);
+        words[k] = add(add(words[k - 16], s0), add(words[k - 7], s1));
+
+    }
+    bitset<32> a, b, c, d, e, f, g, h;
+
+    a = H[0];
+    b = H[1];
+    c = H[2];
+    d = H[3];
+    e = H[4];
+    f = H[5];
+    g = H[6];
+    h = H[7];
+
+
+    for(int t = 0; t < 64; t++)
+    {
+        bitset<32> s1 = rotateRight(e,6) ^ rotateRight(e,11) ^ rotateRight(e,25);
+        bitset<32> temp1 = add( h , add( s1 , add( Ch(e,f,g) , add( constants[t] , words[t] ) ) ) );
+        bitset<32> s0 = rotateRight(e,2) ^ rotateRight(e,13) ^ rotateRight(e,22);
+        bitset<32> temp2 = add(s0, Maj(a,b,c));
+        h = g;
+        g = f;
+        f = e;
+        e = add( d , temp1);
+        d = c;
+        c = b;
+        b = a;
+        a = add(temp1,temp2);
+    }
+    H[0] = add(a,H[0]);
+    H[1] = add(b,H[1]);
+    H[2] = add(c,H[2]);
+    H[3] = add(d,H[3]);
+    H[4] = add(e,H[4]);
+    H[5] = add(f,H[5]);
+    H[6] = add(g,H[6]);
+    H[7] = add(h,H[7]);
+
+    string hashInBin = H[0].to_string() + H[1].to_string() + H[2].to_string()+ H[3].to_string() +
+                        H[4].to_string() + H[5].to_string() + H[6].to_string() + H[7].to_string();
+
+    return binToHex(hashInBin);
+}
+
+
+
 int main()
 {
     string str;
     cin >> str;
 
-    string processed_sms = paddingMessage(str);
+    string processedSms = paddingMessage(str);
+    cout << makingDigest(processedSms) << endl;
+    return 0;
 }
